6560fa25647fe55e7ad7ab86363dfc0f38b3adac
Raghad Abid <66277485+rgd75xq@users.noreply.github.com>
2020-09-25 23:11:01 +0500

    Add files via upload

---
+++ docs/portfolio/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.nav-link1').on('click', function () {
+        $('.top-nav').toggleClass('open');
+        $('.menu-toggler').toggleClass('open');
+    });
+
+    $('.nav-link2').on('click', function () {
+        $('.top-nav').toggleClass('open');
+        $('.menu-toggler').toggleClass('open');
+    });
+
+    $('.nav-link3').on('click', function () {
+        $('.top-nav').toggleClass('open');
+        $('.menu-toggler').toggleClass('open');
+    });
+
+    $('.nav-link4').on('click', function () {
+        $('.top-nav').toggleClass('open');
+        $('.menu-toggler').toggleClass('open');
+    });
+
+    $('.nav-link5').on('click', function () {
+        $('.top-nav').toggleClass('open');
+        $('.menu-toggler').toggleClass('open');
+    });
+});
